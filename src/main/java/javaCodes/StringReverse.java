60c30cfd5036b26e6a873b3187471427d07ed3b6
Naveenjeru <naveenjeru1999@gmail.com>
2024-12-17 12:07:42 +0530

    java code string reverse

--- src/main/java/javaCodes/StringReverse.java
+++ src/main/java/javaCodes/StringReverse.java
@@ -4,10 +4,18 @@ public class StringReverse {
 
 	public static void main(String[] args) {
 		
-
+		String str="naveen is on interview";
+		String[] split=str.split(" ");
+		String line="";
+		for(String s:split) {
+			String word="";
 		
-		
-		
+		for(int i=s.length()-1;i>=0;i-- ) {
+			word=word+s.charAt(i);
+		}
+		line=line + word+" ";
+	}
+	System.out.println(line);	
 	}
 
 }  
