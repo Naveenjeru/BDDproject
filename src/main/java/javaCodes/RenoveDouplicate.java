58c488b9151fe70cd2258ff776d14428d10db2df
Naveenjeru <naveenjeru1999@gmail.com>
2024-12-17 12:24:31 +0530

    set programe

--- src/main/java/javaCodes/RenoveDouplicate.java
+++ src/main/java/javaCodes/RenoveDouplicate.java
@@ -1,10 +1,21 @@
 package javaCodes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RenoveDouplicate {
 
 	public static void main(String[] args) {
+	String str="java c java c# c python";
+	String[] split=str.split(" ");
+	Set<String> set=new HashSet<String>();
+	for(int i=0;i<split.length;i++) {
+		set.add(split[i]);
+	}
+for(String s:set) {
+	System.out.println(s);
+}
 	
-
 	}
 
 }
