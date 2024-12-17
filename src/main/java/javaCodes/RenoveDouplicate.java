e20a5cc5d79d28939ff4c29b06fcfdb5adf2983a
Naveenjeru <naveenjeru1999@gmail.com>
2024-12-17 12:35:04 +0530

    zest comments added

--- src/main/java/javaCodes/RenoveDouplicate.java
+++ src/main/java/javaCodes/RenoveDouplicate.java
@@ -6,17 +6,17 @@ import java.util.Set;
 public class RenoveDouplicate {
 
 	public static void main(String[] args) {
-		String[] split = {"java","c","java","python"};
-	//String str="java c java c# c python";
-//	String[] split=str.split(" ");
-	Set<String> set=new HashSet<String>();
-	for(int i=0;i<split.length;i++) {
-		set.add(split[i]);
-	}
-for(String s:set) {
-	System.out.println(s);
-}
-	
+		String[] split = {"java","c","java","python"};  //jeru
+		//String str="java c java c# c python";			//bandi		
+		//	String[] split=str.split(" ");				//naveen
+		Set<String> set=new HashSet<String>();			//kumar
+		for(int i=0;i<split.length;i++) {				//programe		
+			set.add(split[i]);
+		}
+		for(String s:set) {
+			System.out.println(s);
+		}
+
 	}
 
 }
