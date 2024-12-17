2338a5e53baeb777c942a16ddb38ee993cac37aa
Naveenjeru <naveenjeru1999@gmail.com>
2024-12-17 12:29:07 +0530

    set modified

--- src/main/java/javaCodes/RenoveDouplicate.java
+++ src/main/java/javaCodes/RenoveDouplicate.java
@@ -6,8 +6,9 @@ import java.util.Set;
 public class RenoveDouplicate {
 
 	public static void main(String[] args) {
-	String str="java c java c# c python";
-	String[] split=str.split(" ");
+		String[] split = {"java","c","java","python"};
+	//String str="java c java c# c python";
+//	String[] split=str.split(" ");
 	Set<String> set=new HashSet<String>();
 	for(int i=0;i<split.length;i++) {
 		set.add(split[i]);
