0a0b37657b1b9a7a034deaddcce2bf8754fef502
Naveenjeru <naveenjeru1999@gmail.com>
2024-12-30 23:34:25 +0530

    add

--- src/main/java/javaCodes/AbcClass.java
+++ src/main/java/javaCodes/AbcClass.java
@@ -1,14 +1,24 @@
 package javaCodes;
 
+
+import java.util.*;
+
 public class AbcClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		System.out.println("hello");
+	
+String str="N!a$v#2e1e*n";
+StringBuilder sb=new StringBuilder();
 
+for(int i=0;i<str.length();i++) {
+	char c=str.charAt(i);
+	if(Character.isLetter(c)) {
+		sb.append(c);
+	}
+}
+
+		System.out.println(sb.toString());
 	}
 
 }
